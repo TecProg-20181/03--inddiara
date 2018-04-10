8a163db049261c94d57ffc9612b56201ae6291f0
inddiara <indiara06d@gmail.com>
2018-04-10 17:23:44 -0300

    Function play again or out game added

--- hang.py
+++ hang.py
@@ -3,6 +3,7 @@ import string
 
 WORDLIST_FILENAME = "palavras.txt"
 
+
 def loadWords():
     """
     Depending on the size of the word list, this function may
@@ -15,14 +16,16 @@ def loadWords():
     line = inFile.readline()
     # wordlist: list of strings
     wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
+    print " ", len(wordlist), "words loaded.\n"
     return random.choice(wordlist)
+
 
 def openFile(fileName):
     # inFile: file
     inFile = open(fileName, 'r', 0)
 
     return inFile
+
 
 def isWordGuessed(secretWord, lettersGuessed):
     secretLetters = []
@@ -35,23 +38,24 @@ def isWordGuessed(secretWord, lettersGuessed):
 
     return True
 
-def getGuessedWord():
 
-     guessed = ''
-     return guessed
+def getGuessedWord():
+    guessed = ''
+    return guessed
+
 
 def getAvailableLetters():
-    import string
     # 'abcdefghijklmnopqrstuvwxyz'
     available = string.ascii_lowercase
 
-
     return available
 
-def printMenu():
-    print 'Welcome to the game, Hangam!'
-    print 'I am thinking of a word that is', len(secretWord), ' letters long.'
-    print '-------------'
+
+def printMenu(secretWord):
+    # print 'Welcome to the game, Hangam!'
+    print 'I am thinking of a word that is', len(secretWord), ' letters long.\n'
+    print '------------------------------------------------\n'
+
 
 def updateAvailable(available, lettersGuessed):
     for letter in available:
@@ -66,8 +70,9 @@ def checkGuessed(lettersGuessed, secretWord):
         if letter in lettersGuessed:
             guessed += letter
         else:
-            guessed += '_ '
+            guessed += '_'
     return guessed
+
 
 def checkWin(secretWord, lettersGuessed):
     if isWordGuessed(secretWord, lettersGuessed) == True:
@@ -76,15 +81,16 @@ def checkWin(secretWord, lettersGuessed):
         print 'Sorry, you ran out of guesses. The word was ', secretWord, '.'
 
 
-def hangman(secretWord):
-
+def ifif():
     guesses = 8
     lettersGuessed = []
-    #Calling function to print menu
-    printMenu()
 
-    while  isWordGuessed(secretWord, lettersGuessed) == False and guesses >0:
-        print 'You have ', guesses, 'guesses left.'
+    secretWord = loadWords().lower()
+
+    printMenu(secretWord)
+
+    while isWordGuessed(secretWord, lettersGuessed) == False and guesses > 0:
+        print '\nYou have ', guesses, 'guesses left.'
 
         available = getAvailableLetters()
         updateAvailable(available, lettersGuessed)
@@ -92,27 +98,44 @@ def hangman(secretWord):
         print 'Available letters', available
         letter = raw_input('Please guess a letter: ')
         if letter in lettersGuessed:
-            guessed = checkGuessed(lettersGuessed,secretWord)
+            guessed = checkGuessed(lettersGuessed, secretWord)
 
             print 'Oops! You have already guessed that letter: ', guessed
         elif letter in secretWord:
             lettersGuessed.append(letter)
 
-            guessed = checkGuessed(lettersGuessed,secretWord)
+            guessed = checkGuessed(lettersGuessed, secretWord)
 
-            print 'Good Guess: ', guessed
+            print '\nGood Guess: ', guessed
         else:
-            guesses -=1
+            guesses -= 1
             lettersGuessed.append(letter)
 
-            guessed = checkGuessed(lettersGuessed,secretWord)
+            guessed = checkGuessed(lettersGuessed, secretWord)
 
             print 'Oops! That letter is not in my word: ',  guessed
-        print '------------'
+        print '\n---------------------------------------------\n'
 
     else:
-        checkWin(secretWord,lettersGuessed)
+        checkWin(secretWord, lettersGuessed)
+
+    print('Press 1 to play again')
+    option = raw_input('Press 2 to exit\n')
+    if option == '1':
+        return False
+    elif option == '2':
+        return True
+    else:
+        quit()
 
 
-secretWord = loadWords().lower()
-hangman(secretWord)
+def hangman():
+
+    # Calling function to print menu
+    print 'Welcome to the game, Hangam!'
+    finish = False
+    while finish == False:
+        finish = ifif()
+
+
+hangman()
